fe9d07fed0a00b55d8fb22e06aa5baa24afead16
Richie Arkanoff <78461813+rarky1git@users.noreply.github.com>
2026-03-25 15:43:09 -0400

    Update version number to v0.5.1 in HomePage

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -395,7 +395,7 @@ export default function HomePage() {
         </div>
       )}
       <footer className="absolute bottom-4 text-green-600 text-[11px] font-mono select-none">
-        v0.5
+        v0.5.1
       </footer>
     </div>
   );
